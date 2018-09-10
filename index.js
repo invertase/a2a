e4db7d1359ee3c95e61786f79d4b8ddb59f85fec
Salakar <mike.diarmid@gmail.com>
2018-09-10 22:21:28 +0100

    cleanup & remove redundant functionality

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
-function A2A(oOrP) {
-    if (!oOrP) return Promise.resolve([null, oOrP]);
+/**
+ * Async Await to Array
+ *
+ * @param oOrP objectOrPromise
+ * @returns {*}
+ */
+module.exports = function A2A(oOrP) {
+  if (!oOrP) return Promise.resolve([null, oOrP]);
 
-    // single promise
-    if (oOrP.then) {
-        return oOrP.then((r) => [null, r]).catch((e) => [e, undefined]);
-    }
+  // single promise
+  if (oOrP.then) {
+    return oOrP.then((r) => [null, r]).catch((e) => [e, undefined]);
+  }
 
-    // function that returns a single promise
-    if (typeof oOrP === 'function') {
-        return oOrP()
-            .then((r) => [null, r])
-            .catch((e) => [e, undefined]);
-    }
+  // array of promises
+  if (Array.isArray(oOrP) && oOrP.length && oOrP[0].then) {
+    return Promise.all(oOrP)
+    .then((r) => [null, r])
+    .catch((e) => [e, undefined]);
+  }
 
-    // array of promises
-    if (Array.isArray(oOrP) && oOrP.length && oOrP[0].then) {
-        return Promise.all(oOrP)
-            .then((r) => [null, r])
-            .catch((e) => [e, undefined]);
-    }
+  // non promise values - error
+  if (oOrP instanceof Error) return Promise.resolve([oOrP, undefined]);
 
-    // array of functions that returns a single promise
-    if (Array.isArray(oOrP) && oOrP.length && typeof oOrP[0] === 'function') {
-        return Promise.all(oOrP.map((f) => f()))
-            .then((r) => [null, r])
-            .catch((e) => [e, undefined]);
-    }
-
-    // non promise values - error
-    if (oOrP instanceof Error) return Promise.resolve([oOrP, undefined]);
-
-    // non promise values - any other value
-    return Promise.resolve([null, oOrP]);
-}
-
-module.exports = A2A;
+  // non promise values - any other value
+  return Promise.resolve([null, oOrP]);
+};
--- test/a2a.js
+++ test/a2a.js
@@ -2,126 +2,78 @@ const assert = require('assert');
 const A2A = require('../');
 
 describe('A2A', function () {
-    it('should resolve an undefined value', async function () {
-        const [error, result] = await A2A();
+  it('should resolve an undefined value', async function () {
+    const [error, result] = await A2A();
 
-        assert(error === null);
-        assert(result === undefined);
-    });
+    assert(error === null);
+    assert(result === undefined);
+  });
 
-    it('should resolve a falsey value', async function () {
-        const [error, result] = await A2A('');
+  it('should resolve a non promise value', async function () {
+    const [error, result] = await A2A('');
 
-        assert(error === null);
-        assert(result === '');
-    });
+    assert(error === null);
+    assert(result === '');
+  });
 
-    it('should take a promise a single promise and resolve', async function () {
-       const prom = new Promise(res => res('foo'));
+  it('should take a promise a single promise and resolve', async function () {
+    const prom = new Promise(res => res('foo'));
 
-       const [error, result] = await A2A(prom);
+    const [error, result] = await A2A(prom);
 
-       assert(error === null);
-       assert(result === 'foo');
-    });
+    assert(error === null);
+    assert(result === 'foo');
+  });
 
-    it('should take a promise a single promise and reject', async function () {
-        const prom = new Promise((res, rej) => rej(
-            new Error('bar')
-        ));
+  it('should take a promise a single promise and reject', async function () {
+    const prom = new Promise((res, rej) => rej(
+      new Error('bar')
+    ));
 
-        const [error, result] = await A2A(prom);
+    const [error, result] = await A2A(prom);
 
-        assert(result === undefined);
-        assert(error.message === 'bar');
-    });
+    assert(result === undefined);
+    assert(error.message === 'bar');
+  });
 
-    it('should resolve a callable promise', async function () {
-        const prom = () => new Promise(res => res('foo'));
+  it('should resolve an array of promises', async function () {
+    const prom = [
+      Promise.resolve('foo'),
+      Promise.resolve('bar'),
+    ];
 
-        const [error, result] = await A2A(prom);
+    const [error, result] = await A2A(prom);
 
-        assert(error === null);
-        assert(result === 'foo');
-    });
+    assert(error === null);
+    assert(Array.isArray(result));
+    assert.deepStrictEqual(result, [
+      'foo', 'bar',
+    ]);
+  });
 
-    it('should reject a callable promise', async function () {
-        const prom = () => new Promise((res, rej) => rej(
-            new Error('bar')
-        ));
+  it('should reject an array of promises', async function () {
+    const prom = [
+      Promise.resolve('foo'),
+      Promise.reject(new Error('foobar')),
+    ];
 
-        const [error, result] = await A2A(prom);
+    const [error, result] = await A2A(prom);
 
-        assert(result === undefined);
-        assert(error.message === 'bar');
-    });
+    assert(result === undefined);
+    assert(error.message === 'foobar');
+  });
 
-    it('should resolve an array of promises', async function () {
-        const prom = [
-            Promise.resolve('foo'),
-            Promise.resolve('bar'),
-        ];
+  it('should resolve an error value', async function () {
+    const [error, result] = await A2A(new Error('error'));
 
-        const [error, result] = await A2A(prom);
+    assert(result === undefined);
+    assert(error.message === 'error');
+  });
 
-        assert(error === null);
-        assert(Array.isArray(result));
-        assert.deepStrictEqual(result, [
-            'foo', 'bar',
-        ]);
-    });
+  it('should resolve a none promise value', async function () {
+    const [error, result] = await A2A(1337);
 
-    it('should reject an array of promises', async function () {
-        const prom = [
-            Promise.resolve('foo'),
-            Promise.reject(new Error('foobar')),
-        ];
-
-        const [error, result] = await A2A(prom);
-
-        assert(result === undefined);
-        assert(error.message === 'foobar');
-    });
-
-    it('should resolve an array of callable promises', async function () {
-        const prom = [
-            () => Promise.resolve('foo'),
-            () => Promise.resolve('bar'),
-        ];
-
-        const [error, result] = await A2A(prom);
-
-        assert(error === null);
-        assert(Array.isArray(result));
-        assert.deepStrictEqual(result, [
-            'foo', 'bar',
-        ]);
-    });
-
-    it('should reject if a single promise fails', async function () {
-        const prom = [
-            () => Promise.resolve('foo'),
-            () => Promise.resolve('bar'),
-            () => Promise.reject(new Error('foobar')),
-        ];
-
-        const [error, result] = await A2A(prom);
-
-        assert(result === undefined);
-        assert(error.message === 'foobar');
-    });
-
-    it('should resolve an error value', async function () {
-        const [error, result] = await A2A(new Error('error'));
-
-        assert(result === undefined);
-        assert(error.message === 'error');
-    });
-
-    it('should resolve a none promise value', async function () {
-        const [error, result] = await A2A(1337);
-
-        assert(error === null);
-        assert(result === 1337);
-    });
+    assert(error === null);
+    assert(result === 1337);
+  });
 });
