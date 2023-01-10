23c94da83809931390e52a22239ab69a5b35a9a4
Elliot Hesp <elliot.hesp@gmail.com>
2023-01-10 10:11:26 +0000

    fix: typescript callable type

--- index.d.ts
+++ index.d.ts
@@ -4,4 +4,4 @@ type A2AResult<TResult, TError> = Tuple<TError|null, TResult|undefined>;
 declare function a2a<TResult=any, TError = any>(promise: Promise<TResult>): Promise<A2AResult<TResult, TError>>;
 declare function a2a<TResult=any, TError = any>(promise: Promise<TResult>[]): Promise<A2AResult<TResult[], TError>>;
 
-export default a2a;
+export = a2a;
